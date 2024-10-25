60059fb171488ebcc5e040c4aefec1abda135d89
deboradum <pvwpvw@protonmail.com>
2024-10-25 12:00:03 +0200

    model changes

--- Model.py
+++ Model.py
@@ -5,37 +5,53 @@ from datasetGen.constants import INPUT_DIM, BIN_SIZE
 
 
 class ChessNet(nn.Module):
-    def __init__(self, seq_len):
+    def __init__(self):
         super().__init__()
         dims = 512
         self.l1 = nn.Linear(INPUT_DIM, dims)
+        self.relu1 = nn.ReLU()
+
         self.trans_decoder = nn.TransformerDecoder(
             num_layers=8,
             dims=dims,
             num_heads=8,
         )
-        self.pooling = nn.AvgPool1d(seq_len)
-        self.l2 = nn.Linear(dims, 1024)
-        self.l3 = nn.Linear(1024, 2048)
-        self.l4 = nn.Linear(2048, 2048)
-        self.l5 = nn.Linear(2048, 2048)
+        # self.pooling = nn.AvgPool1d(1)
+        self.l2 = nn.Linear(dims, 2048)
+        self.relu2 = nn.ReLU()
+        self.l3 = nn.Linear(2048, 512)
+        self.relu3 = nn.ReLU()
+
+        self.trans_decoder2 = nn.TransformerDecoder(
+            num_layers=8,
+            dims=512,
+            num_heads=8,
+        )
+
+        self.l4 = nn.Linear(512, 4096)
+        self.relu4 = nn.ReLU()
+        self.l5 = nn.Linear(4096, BIN_SIZE)
+
         self.softmax = nn.Softmax()
 
     def __call__(self, x):
-        x = self.l1(x)
+        x = mx.squeeze(x, 0)
+        x = self.relu1(self.l1(x))
 
+        x = mx.expand_dims(x, 0)
         x = self.trans_decoder(x, x, None, None)
-
-        x = mx.transpose(x, [1, 0, 2])
-        x = self.pooling(x)
-        x = mx.transpose(x, [1, 0, 2])
         x = mx.squeeze(x, 0)
 
-        x = self.l2(x)
-        x = self.l3(x)
-        x = self.l4(x)
+        x = self.relu2(self.l2(x))
+        x = self.relu3(self.l3(x))
+
+        x = mx.expand_dims(x, 0)
+        x = self.trans_decoder2(x, x, None, None)
+        x = mx.squeeze(x, 0)
+
+        x = self.relu4(self.l4(x))
         x = self.l5(x)
 
-        x = self.softmax(x)
+        # x = self.softmax(x)
 
         return x
