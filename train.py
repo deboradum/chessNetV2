69b6b4269deb81f1b10cf10907557a55910f3dd0
deboradum <pvwpvw@protonmail.com>
2024-10-29 14:45:34 +0100

    updated train script

--- train.py
+++ train.py
@@ -1,114 +1,122 @@
 import time
 
+import numpy as np
 import mlx.nn as nn
 import mlx.core as mx
 import mlx.data as dx
 import mlx.optimizers as optim
 
-# from Model import ChessNet
-from ModelV2 import ChessNet
+from Model import ChessNet
 
 from datasetGen.factories import buildinWinsIterableFactory
 
-# batch_size = 2048
 
-# test_dset = (
-#     dx.stream_python_iterable(buildinWinsIterableFactory("datasetGen/builtinWinsTest.db"))
-#     .batch(batch_size)
-# )
-# valid_dset = (
-#     dx.stream_python_iterable(buildinWinsIterableFactory("datasetGen/builtinWinsVal.db"))
-#     .batch(batch_size)
-# )
-
-
-def test(model, dset, eval_fn, num_batches=-1):
+def test(model, dset, batch_size, eval_fn, num_batches=-1):
     accs = []
+    dset = dset.batch(batch_size)
     for i, batch in enumerate(dset):
         if i > num_batches and num_batches != -1:
-            print("breaking")
             break
-        X, y = batch["x"], batch["y"]
+        X, y = mx.array(batch["x"]), mx.array(batch["y"])
         accs.append(eval_fn(model, X, y))
-    print("accs:", mx.array(accs).mean)
+
+    return np.array(accs).mean()
+
+
+def init_log_file(filepath):
+    with open(filepath, "w") as f:
+        f.write("epoch,batch,train_loss,train_acc,test_acc\n")
+
+
+def log_loss_and_acc(
+    filepath, epoch, batch, avg_train_loss, avg_train_acc, avg_test_acc, time_taken
+):
+    print(
+        f"Epoch: {epoch}, batch: {batch} | train loss: {avg_train_loss:.2f} | train acc: {avg_train_acc:.2f} | test acc: {avg_test_acc:.2f} | Took {time_taken:.2f} seconds"
+    )
+    with open(filepath, "a+") as f:
+        f.write(f"{epoch},{batch},{avg_train_loss},{avg_train_acc}\n")
 
 
 def train(
-    model, train_dset, val_dset, optimizer, loss_fn, eval_fn, nepochs, batch_size, log_interval=10
+    model,
+    train_dset,
+    val_dset,
+    optimizer,
+    loss_fn,
+    eval_fn,
+    nepochs,
+    batch_size,
+    log_path,
+    log_interval=10,
 ):
     loss_and_grad_fn = nn.value_and_grad(model, loss_fn)
+    init_log_file(log_path)
+
     for epoch in range(nepochs):
-        train_dset = (
-            dx.stream_python_iterable(
-                buildinWinsIterableFactory("datasetGen/builtinWinsTrainOVERFIT.db")
-            )
-            .batch(batch_size)
-            .shuffle(4096)
-        )
-        for batch in train_dset:
+        losses = []
+        accs = []
+
+        train_dset = train_dset.batch(batch_size).shuffle(4096)
+
+        start = time.perf_counter()
+        for i, batch in enumerate(train_dset):
             X, y = mx.array(batch["x"]), mx.array(batch["y"])
             loss, grads = loss_and_grad_fn(model, X, y)
             acc = eval_fn(model, X, y)
-            print("loss:", float(loss), "acc:", float(acc))
+            losses.append(loss)
+            accs.append(acc)
+
+            if i % log_interval == 0:
+                test_acc = test(model, val_dset, batch_size, eval_fn, num_batches=50)
+                stop = time.perf_counter()
+                time_taken = round(stop-start, 2)
+                log_loss_and_acc(
+                    log_path,
+                    epoch,
+                    i,
+                    round(np.array(losses).mean(), 2),
+                    round(np.array(accs).mean(), 2),
+                    round(test_acc, 2),
+                    time_taken
+                )
+                losses = []
+                accs = []
+                start = time.perf_counter()
+
             optimizer.update(model, grads)
             mx.eval(model.parameters(), optimizer.state)
-            if float(acc) > 0.9:
-                print("Converges in ", epoch, "epochs.")
-                return
-    print("Did not converge")
-        # test(model, val_dset, eval_fn, num_batches=100)
+
+
+def loss_fn(model, X, y):
+    return mx.mean(nn.losses.cross_entropy(model(X), y))
+
+
+def eval_fn(model, X, y):
+    return mx.mean(mx.argmax(model(X), axis=1) == y)
 
 
 if __name__ == "__main__":
+    mx.random.seed(101)
 
-    def loss_fn(model, X, y):
-        return mx.mean(nn.losses.cross_entropy(model(X), y))
+    train_overfit_dset = dx.stream_python_iterable(
+        buildinWinsIterableFactory("datasetGen/builtinWinsTrainOVERFIT.db")
+    )
+    train_dset = dx.stream_python_iterable(
+        buildinWinsIterableFactory("datasetGen/builtinWinsTrain.db")
+    )
+    test_dset = dx.stream_python_iterable(
+        buildinWinsIterableFactory("datasetGen/builtinWinsTest.db")
+    )
+    valid_dset = dx.stream_python_iterable(
+        buildinWinsIterableFactory("datasetGen/builtinWinsVal.db")
+    )
 
-    def eval_fn(model, X, y):
-        return mx.mean(mx.argmax(model(X), axis=1) == y)
+    lr = 0.00005
+    optimizer = optim.Adam(lr)
+    nepochs = 5
+    net = ChessNet(8, 8, 128, 512)
+    batch_size = 512
 
-
-
-optimizers = {
-    "adam": [
-        # 0.00001,
-        0.0001,
-    ],
-    # "adagrad": [
-    #     0.00075,
-    #     0.0005,
-    # ],
-    # "sgd": [
-    #     0.00075,
-    #     0.0005,
-    #     0.0005,
-    #     0.001,
-    #     0.001,
-    #     0.005,
-    #     0.01,
-    #     0.05,
-    #     0.1,
-    # ],
-}
-
-# Loop through each optimizer and its corresponding learning rates
-for optimizer_name, learning_rates in optimizers.items():
-    for batch_size in [256]:
-        for lr in learning_rates:
-            net = ChessNet(4, 4, 128, 512)
-            print(f"{optimizer_name}, {lr}, {batch_size}")
-
-            # Choose the optimizer based on its name
-            if optimizer_name == "adam":
-                optimizer = optim.Adam(lr)
-            elif optimizer_name == "adagrad":
-                optimizer = optim.Adagrad(lr)
-            elif optimizer_name == "sgd":
-                optimizer = optim.SGD(lr)
-
-            tic = time.perf_counter()
-            train(net, "train_dset", "valid_dset", optimizer, loss_fn, eval_fn, 500, batch_size)
-            toc = time.perf_counter()
-            taken = round(toc-tic, 2)
-            print("Took", taken, "seconds")
-            print()
+    print(f"Training chessNet with op")
+    train(net, train_dset, valid_dset, optimizer, loss_fn, eval_fn, nepochs, batch_size, "adam_5e-5_512bs.csv")
