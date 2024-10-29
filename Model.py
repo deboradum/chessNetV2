f1cfe87f95de7aaffbbf8173d4c0452586f765d7
deboradum <pvwpvw@protonmail.com>
2024-10-29 09:00:11 +0100

    remove old model

--- Model.py
+++ Model.py
@@ -4,54 +4,80 @@ import mlx.nn as nn
 from datasetGen.constants import INPUT_DIM, BIN_SIZE
 
 
-class ChessNet(nn.Module):
-    def __init__(self):
+class FeedForward(nn.Module):
+    def __init__(self, embedding_dim, widening_factor=4):
         super().__init__()
-        dims = 512
-        self.l1 = nn.Linear(INPUT_DIM, dims)
-        self.relu1 = nn.ReLU()
 
-        self.trans_decoder = nn.TransformerDecoder(
-            num_layers=8,
-            dims=dims,
-            num_heads=8,
-        )
-        # self.pooling = nn.AvgPool1d(1)
-        self.l2 = nn.Linear(dims, 2048)
-        self.relu2 = nn.ReLU()
-        self.l3 = nn.Linear(2048, 512)
-        self.relu3 = nn.ReLU()
+        dim = embedding_dim * widening_factor
 
-        self.trans_decoder2 = nn.TransformerDecoder(
-            num_layers=8,
-            dims=512,
-            num_heads=8,
-        )
-
-        self.l4 = nn.Linear(512, 4096)
-        self.relu4 = nn.ReLU()
-        self.l5 = nn.Linear(4096, BIN_SIZE)
-
-        self.softmax = nn.Softmax()
+        self.l1 = nn.Linear(embedding_dim, dim, bias=False)
+        self.l2 = nn.Linear(embedding_dim, dim, bias=False)
+        self.silu = nn.SiLU()
+        self.final = nn.Linear(dim, embedding_dim, bias=False)
 
     def __call__(self, x):
-        x = mx.squeeze(x, 0)
-        x = self.relu1(self.l1(x))
+        x1 = self.l1(x)
+        x2 = self.l2(x)
+        x = self.silu(x1) * x2
 
-        x = mx.expand_dims(x, 0)
-        x = self.trans_decoder(x, x, None, None)
-        x = mx.squeeze(x, 0)
+        return self.final(x)
 
-        x = self.relu2(self.l2(x))
-        x = self.relu3(self.l3(x))
 
-        x = mx.expand_dims(x, 0)
-        x = self.trans_decoder2(x, x, None, None)
-        x = mx.squeeze(x, 0)
+class TransformerBlock(nn.Module):
+    def __init__(self, embedding_dim, num_heads):
+        super().__init__()
+        self.layerNorm1 = nn.LayerNorm(embedding_dim)
+        self.attention = nn.MultiHeadAttention(
+            embedding_dim,
+            num_heads,
+        )
+        self.layerNorm2 = nn.LayerNorm(embedding_dim)
+        self.feedForward = FeedForward(embedding_dim)
 
-        x = self.relu4(self.l4(x))
-        x = self.l5(x)
+    def __call__(self, x):
+        attention_input = self.layerNorm1(x)
+        att = self.attention(attention_input, attention_input, attention_input)
+        x += att
 
-        # x = self.softmax(x)
+        mlp_input = self.layerNorm2(x)
+        mlp_output = self.feedForward(mlp_input)
+        x += mlp_output
 
         return x
+
+
+class ChessNet(nn.Module):
+    def __init__(self, num_layers, num_heads, vocab_size, embed_dim, max_seq_len=86):
+        super().__init__()
+        self.token_embeddings = nn.Embedding(vocab_size, embed_dim)
+        self.positional_embeddings = nn.Embedding(max_seq_len, embed_dim)
+        self.postLayerNorm = False
+
+        self.layers = []
+        for _ in range(num_layers):
+            self.layers.append(TransformerBlock(embed_dim, 8))
+
+        self.final_layer = nn.Linear(embed_dim, BIN_SIZE)
+
+    def __call__(self, x):
+        # TODO: shift enzo
+        b, seq_len = x.shape
+        h = self.token_embeddings(x)
+
+        positions = mx.arange(seq_len).reshape(1, seq_len)  # Create a 1D array of shape (1, seq_len)
+        positions = mx.tile(positions, (b, 1))  # Repeat the array b times to create shape (b, seq_len)
+
+        h += self.positional_embeddings(positions)
+
+        for layer in self.layers:
+            h = layer(h)
+
+        # h = mx.mean(h, axis=1)
+
+        if self.postLayerNorm:
+            h = self.postLayerNorm(h)
+
+        logits = self.final_layer(h)
+        # print(logits[:, -1, :].shape)
+
+        return logits[:, -1, :]
