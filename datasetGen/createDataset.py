d06c27fae9aa65b6427b056d528a250b0a28d646
deboradum <pvwpvw@protonmail.com>
2024-11-08 12:45:47 +0100

    multithreaded datasetgen

--- datasetGen/createDataset.py
+++ datasetGen/createDataset.py
@@ -3,8 +3,8 @@ import math
 import json
 import sqlite3
 import chess.pgn
-
 from stockfish import Stockfish
+from concurrent.futures import ThreadPoolExecutor
 
 MAX_ASCII = 119
 MIN_ASCII = 45
@@ -27,22 +27,16 @@ def extract_eval_from_comment(comment):
 
 def pad_fen(fen):
     parts = fen.split()
-
     # 1. Piece placement (pad to 72 characters)
     piece_placement = parts[0].ljust(72, ".")
-
     # 2. Active color (already 1 character, no padding needed)
     active_color = parts[1]
-
     # 3. Castling availability (pad to 4 characters)
     castling_availability = parts[2].ljust(4, ".")
-
     # 4. En passant target square (pad to 2 characters)
     en_passant = parts[3].ljust(2, ".")
-
     # 5. Halfmove clock (pad to 3 digits)
     halfmove_clock = parts[4].zfill(3)
-
     # 6. Fullmove number (pad to 4 digits)
     fullmove_number = parts[5].zfill(4)
 
@@ -62,16 +56,14 @@ def get_all_pgn_files(root_dir, max_depth=10):
                 search_directory(entry.path, current_depth + 1)
 
     search_directory(root_dir, 0)
-
     return files
 
 
 def get_stockfish_eval(fen, stockfish):
     stockfish.set_fen_position(fen)
     evaluation = stockfish.get_evaluation()
-
     if evaluation["type"] == "cp":
-        return evaluation["value"]/100
+        return evaluation["value"] / 100
     elif evaluation["type"] == "mate":
         mate_score = 10000 * (1 if evaluation["value"] > 0 else -1)
         return mate_score
@@ -83,7 +75,7 @@ def norm_ascii(ascii_list):
     return [round((x - MIN_ASCII) / (MAX_ASCII - MIN_ASCII), 5) for x in ascii_list]
 
 
-def main(filepaths):
+def process_file(filepath):
     conn = sqlite3.connect("dataset.db")
     conn.execute(
         """CREATE TABLE IF NOT EXISTS positions (
@@ -95,73 +87,75 @@ def main(filepaths):
                active_bin_64 INT,
                ascii_codes TEXT,
                norm_ascii_codes TEXT,
-               stockfish_eval_15 REAL,
-               stockfish_win_perc_15 REAL
+               stockfish_eval_20 REAL,
+               stockfish_win_perc_20 REAL
            )"""
     )
 
-    stockfish = Stockfish("/opt/homebrew/bin/stockfish", depth=15)
+    stockfish = Stockfish("/opt/homebrew/bin/stockfish", depth=20)
     stockfish.set_skill_level(20)
 
-    for filepath in filepaths:
-        pgn = open(filepath)
+    pgn = open(filepath)
+    g = chess.pgn.read_game(pgn)
+    while True:
+        positions = []
         g = chess.pgn.read_game(pgn)
-        # Read all games in the file
-        while True:
-            positions = []
-            g = chess.pgn.read_game(pgn)
-            if not g:
-                break
+        if not g:
+            break
 
-            board = g.board()
-            for node in g.mainline():
-                move = node.move
-                eval_value = extract_eval_from_comment(node.comment)
+        board = g.board()
+        for node in g.mainline():
+            move = node.move
+            eval_value = extract_eval_from_comment(node.comment)
+            board.push(move)
 
-                board.push(move)
+            if eval_value is None:
+                continue
 
-                if eval_value is None:
-                    continue
+            if not board.turn:
+                eval_value = -eval_value
 
-                if not board.turn:
-                    eval_value = -eval_value
+            fen = board.fen()
+            padded_fen = pad_fen(fen)
+            win_perc = get_win_perc(eval_value * 100)
+            active_bin_128 = min(int(win_perc / (1 / 128)), 127)
+            active_bin_64 = min(int(win_perc / (1 / 64)), 127)
+            ascii_list = [ord(c) for c in padded_fen]
+            ascii_codes = json.dumps(ascii_list)
+            norm_ascii_codes = json.dumps(norm_ascii(ascii_list))
+            stockfish_eval_20 = get_stockfish_eval(fen, stockfish)
 
-                fen = board.fen()
-                padded_fen = pad_fen(fen)
-                win_perc = get_win_perc(eval_value * 100)
-                active_bin_128 = min(int(win_perc / (1 / 128)), 127)
-                active_bin_64 = min(int(win_perc / (1 / 64)), 127)
-                ascii_list = [ord(c) for c in padded_fen]
-                ascii_codes = json.dumps(ascii_list)
-                norm_ascii_codes = json.dumps(norm_ascii(ascii_list))
-                stockfish_eval_15 = get_stockfish_eval(fen, stockfish)
+            if not board.turn:
+                stockfish_eval_20 = -stockfish_eval_20
+            stockfish_win_perc_20 = get_win_perc(stockfish_eval_20 * 100)
 
-                if not board.turn:
-                    stockfish_eval_15 = -stockfish_eval_15
-                stockfish_win_perc_15 = get_win_perc(stockfish_eval_15 * 100)
-
-                positions.append(
-                    (
-                        fen,
-                        padded_fen,
-                        eval_value,
-                        win_perc,
-                        active_bin_128,
-                        active_bin_64,
-                        ascii_codes,
-                        norm_ascii_codes,
-                        stockfish_eval_15,
-                        stockfish_win_perc_15,
-                    )
+            positions.append(
+                (
+                    fen,
+                    padded_fen,
+                    eval_value,
+                    win_perc,
+                    active_bin_128,
+                    active_bin_64,
+                    ascii_codes,
+                    norm_ascii_codes,
+                    stockfish_eval_20,
+                    stockfish_win_perc_20,
                 )
-
-            cursor = conn.cursor()
-            cursor.executemany(
-                "INSERT OR IGNORE INTO positions (fen, padded_fen, eval_value, win_perc, active_bin_128, active_bin_64, ascii_codes, norm_ascii_codes, stockfish_eval_15, stockfish_win_perc_15) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-                positions,
             )
-            conn.commit()
+
+        cursor = conn.cursor()
+        cursor.executemany(
+            "INSERT OR IGNORE INTO positions (fen, padded_fen, eval_value, win_perc, active_bin_128, active_bin_64, ascii_codes, norm_ascii_codes, stockfish_eval_20, stockfish_win_perc_20) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+            positions,
+        )
+        conn.commit()
     conn.close()
+
+
+def main(filepaths):
+    with ThreadPoolExecutor(max_workers=8) as executor:
+        executor.map(process_file, filepaths)
 
 
 paths = get_all_pgn_files("../data")
