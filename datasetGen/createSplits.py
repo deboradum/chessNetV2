851f4f8d6fc01db775dbf7b3b09c78cd89013eaa
deboradum <pvwpvw@protonmail.com>
2024-11-09 18:14:25 +0100

    splits for new db format

--- datasetGen/createSplits.py
+++ datasetGen/createSplits.py
@@ -8,10 +8,14 @@ def create_database(db_name):
         """CREATE TABLE IF NOT EXISTS positions (
                fen TEXT PRIMARY KEY,
                padded_fen TEXT,
+               eval_value REAL,
                win_perc REAL,
                active_bin_128 INT,
                active_bin_64 INT,
-               ascii_codes TEXT
+               ascii_codes TEXT,
+               norm_ascii_codes TEXT,
+               stockfish_eval_20 REAL,
+               stockfish_win_perc_20 REAL
            )"""
     )
     return conn
@@ -21,10 +25,10 @@ def split_database(input_db):
     conn = sqlite3.connect(input_db)
     cursor = conn.cursor()
 
-    train_conn = create_database("builtinWinsTrain.db")
-    val_conn = create_database("builtinWinsVal.db")
-    test_conn = create_database("builtinWinsTest.db")
-    overfit_conn = create_database("builtinWinsOverfit.db")
+    train_conn = create_database("datasetTrain.db")
+    val_conn = create_database("datasetVal.db")
+    test_conn = create_database("datasetTest.db")
+    overfit_conn = create_database("datasetOverfit.db")
 
     train_count, val_count, test_count, overfit_count = 0, 0, 0, 0
     batch_size = 8192
@@ -40,28 +44,28 @@ def split_database(input_db):
                 # 80% chance for train
                 train_count += 1
                 train_conn.execute(
-                    "INSERT INTO positions (fen, padded_fen, win_perc, active_bin_128, active_bin_64, ascii_codes) VALUES (?, ?, ?, ?, ?, ?)",
+                    "INSERT INTO positions (fen, padded_fen, eval_value, win_perc, active_bin_128, active_bin_64, ascii_codes, norm_ascii_codes, stockfish_eval_20, stockfish_win_perc_20) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
                     row,
                 )
             elif rand_choice < 0.9:
                 # 10% chance for validation
                 val_count += 1
                 val_conn.execute(
-                    "INSERT INTO positions (fen, padded_fen, win_perc, active_bin_128, active_bin_64, ascii_codes) VALUES (?, ?, ?, ?, ?, ?)",
+                    "INSERT INTO positions (fen, padded_fen, eval_value, win_perc, active_bin_128, active_bin_64, ascii_codes, norm_ascii_codes, stockfish_eval_20, stockfish_win_perc_20) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
                     row,
                 )
             else:
                 # 10% chance for test
                 test_count += 1
                 test_conn.execute(
-                    "INSERT INTO positions (fen, padded_fen, win_perc, active_bin_128, active_bin_64, ascii_codes) VALUES (?, ?, ?, ?, ?, ?)",
+                    "INSERT INTO positions (fen, padded_fen, eval_value, win_perc, active_bin_128, active_bin_64, ascii_codes, norm_ascii_codes, stockfish_eval_20, stockfish_win_perc_20) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
                     row,
                 )
             if rand_choice < 0.005 and overfit_count<8192:
                 # .5% chance for overfit db, used for testing
                 overfit_count += 1
                 overfit_conn.execute(
-                    "INSERT INTO positions (fen, padded_fen, win_perc, active_bin_128, active_bin_64, ascii_codes) VALUES (?, ?, ?, ?, ?, ?)",
+                    "INSERT INTO positions (fen, padded_fen, eval_value, win_perc, active_bin_128, active_bin_64, ascii_codes, norm_ascii_codes, stockfish_eval_20, stockfish_win_perc_20) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
                     row,
                 )
 
@@ -91,5 +95,5 @@ def split_database(input_db):
     )
 
 
-input_database = "builtinWins.db"
+input_database = "dataset.db"
 split_database(input_database)
